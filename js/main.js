ff2d87d735d084212af3b3c92eadf0dc651eb8e9
Elidallana Cristancho <cristanchodayana062017@gmial.com>
2024-09-23 23:01:50 -0500

    feat: :sparkles: :Diseño de la pagina

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Valores de los medidores
+const values = {
+  'fill-1': 2.69,   // PPO2
+  'fill-2': 2.41,   // CABIN TEMP
+  'fill-3': 14.00,  // CABIN PRESSURE
+  'fill-4': 0.07,   // CO2
+  'fill-5': 26.53,  // LOOP A
+  'fill-6': 20.00,  // LOOP B
+  'fill-7': 0.00,   // NET PWR 1
+  'fill-8': 0.00    // NET PWR 2
+};
+
+function setManometro(id, value, maxValue) {
+  const manometro = document.getElementById(id);
+  const dashoffset = 251 - (value / maxValue) * 251;
+  manometro.style.strokeDashoffset = dashoffset;
+  document.getElementById('value-' + id.split('-')[1]).textContent = value.toFixed(2);
+}
+
+function updateManometro() {
+  setManometro('fill-1', values['fill-1'], 15); // PPO2
+  setManometro('fill-2', values['fill-2'], 50); // CABIN TEMP
+  setManometro('fill-3', values['fill-3'], 30); // CABIN PRESSURE
+  setManometro('fill-4', values['fill-4'], 1);  // CO2
+  setManometro('fill-5', values['fill-5'], 50); // LOOP A
+  setManometro('fill-6', values['fill-6'], 50); // LOOP B
+  setManometro('fill-7', values['fill-7'], 100); // NET PWR 1
+  setManometro('fill-8', values['fill-8'], 100); // NET PWR 2
+}
+
+updateManometro(); // Ejecutar al cargar
