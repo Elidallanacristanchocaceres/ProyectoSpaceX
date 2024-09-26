f4016fd32565e6fcaa8a8e0e4049a7ddf56f9ba7
Elidallana Cristancho <cristanchodayana062017@gmail.com>
2024-09-26 16:54:46 -0500

    feat: :sparkles: Texto

--- js/modules/rockets.js
+++ js/modules/rockets.js
@@ -1,22 +1,16 @@
-export const getAllRockets = async () => {
-    // Este es un simple llamado a la API donde se obtienen los cohetes (rockets).
-    // Se guarda la respuesta de la API en la variable 'res'.
-    // Luego, se convierte la respuesta a formato JSON y se almacena en 'data'.
-    // Finalmente, se retorna 'data' que contiene la información de los cohetes.
-    let res = await fetch("https://api.spacexdata.com/v4/rockets");
+export const getAllRockets = async ()=>{
+    let res = await fetch("https://api.spacexdata.com/v4/rockets")
     let data = await res.json();
     return data;
 }
-
 export const getAllRocketsId = async (id)=>{
     let res = await fetch(`https://api.spacexdata.com/v4/rockets/${id}`)
     let data = await res.json();
     return data;
 }
-export const getRocketMassTotal = async () => {
-    // Configuración para la solicitud POST a la API de SpaceX.
+export const getRocketMassTotal  = async() =>{
     let config = {
-        headers: {
+        headers:{
             "content-type": "application/json"
         },
         method: "POST",
@@ -26,57 +20,44 @@ export const getRocketMassTotal = async () => {
                     "name": 1,
                     "mass": 1
                 },
-                "sort": {
-                    "mass.kg": "desc" // Ordena los cohetes por masa en kilogramos, de mayor a menor.
+                "sort":{
+                    "mass.kg": "desc"
                 },
-                "limit": 1 // Limita el resultado a solo el cohete más pesado.
+                "limit": 1
             }
         })
-    };
-    // Realiza la solicitud a la API y espera la respuesta.
+    }
     let res = await fetch("https://api.spacexdata.com/v4/rockets/query", config);
-    // Extrae la masa del cohete más pesado de la respuesta JSON.
-    let { docs: [{ mass } = {}] } = await res.json();
-    // Retorna la masa del cohete más pesado.
+    let {docs:[{mass} = maxMassRocket]} = await res.json();
     return mass;
 }
-
-export const getRocketPayloadWeightsTotal = async () => {
-    // Configuración para la solicitud POST a la API de SpaceX.
+export const getRocketPayloadWeightsTotal  = async() =>{
     let config = {
-        headers: {
+        headers:{
             "content-type": "application/json"
         },
         method: "POST",
         body: JSON.stringify({
+        
             "options": {
                 "select": {
-                    "name": 1, // Selecciona el nombre del cohete.
-                    "payload_weights": 1 // Selecciona los pesos de carga.
+                    "name": 1,
+                    "payload_weights": 1
                 },
-                "sort": {
-                    "payload_weights.kg": "desc" // Ordena los cohetes por peso de carga en 
-                                                //  kilogramos, de mayor a menor.
-                }
+                "sort":{
+                    "payload_weights.kg": "desc"
+                },
             }
         })
-    };
-    // Realiza la solicitud a la API y espera la respuesta.
+    }
     let res = await fetch("https://api.spacexdata.com/v4/rockets/query", config);
-    // Extrae los documentos de la respuesta JSON.
-    let { docs } = await res.json();
-    // Inicializa un array para almacenar los pesos de carga.
-    let data = [];
-    // Recorre los documentos y acumula los pesos de carga en el array 'data'.
-    docs.forEach(val => data.push(...val.payload_weights));
-    // Ordena el array 'data' por peso de carga en orden descendente.
-    data.sort((a, b) => b.kg - a.kg);
-    // Extrae el peso de carga máximo del primer elemento del array ordenado.
+    let {docs} = await res.json();
+    let data = []
+    docs.forEach(val => data.push(...val.payload_weights))
+    data.sort((a,b) => b.kg - a.kg)
     let [maxPayloadWeightsRocket] = data;
-    // Retorna el peso de carga máximo.
     return maxPayloadWeightsRocket;
 }
-
 export const getRocketHeightTotal  = async() =>{
     let config = {
         headers:{
