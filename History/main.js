d2a4cc0ab0c7630410d43b82e87c1fbb8cba9990
Elidallana Cristancho <cristanchodayana062017@gmial.com>
2024-09-29 16:46:43 -0500

    feat: :sparkles: Agregue imagenes

--- History/main.js
+++ History/main.js
@@ -13,9 +13,6 @@ async function obtenerHistoria() {
 }
 
 // Función para mostrar los eventos históricos en el DOM
-// Función para mostrar los eventos históricos en el DOM
-// Función para mostrar los eventos históricos en el DOM
-// Función para mostrar los eventos históricos en el DOM
 function mostrarHistoria(historia) {
   const historySection = document.getElementById('history');
   
@@ -33,18 +30,61 @@ function mostrarHistoria(historia) {
     const eventDate = new Date(evento.event_date_utc);
     date.textContent = `Fecha del evento: ${eventDate.toLocaleDateString()}`;
     
-    // Agregar imágenes específicas para el primer y segundo evento
+    // Agregar imágenes específicas para los eventos 1, 2, 3 y 4
     const image = document.createElement('img');
     image.style.height = '150px'; // Ajustar el tamaño de la imagen aquí
     image.style.width = '100%'; // Mantener el ancho al 100%
     
     if (index === 0) {
       image.src = 'https://sxcontent9668.azureedge.us/cms-assets/assets/Starship_Flight3_Launch_Remote_Gateway_129_A9251_5b57c3c6be.jpg';
-      image.alt = 'Imagen del primer evento de SpaceX';
+      image.alt = 'Falcon reaches Earth orbit';
     } else if (index === 1) {
       image.src = 'https://sxcontent9668.azureedge.us/cms-assets/assets/Crew_9_outsideloop_IMG_9794_desktop_6efa2efe57.jpg';
-      image.alt = 'Imagen del segundo evento de SpaceX';
-    } else {
+      image.alt = 'Falcon delivers payload to orbit';
+    } else if (index === 2) {
+      image.src = 'https://ichef.bbci.co.uk/ace/standard/226/mcs/media/images/48013000/gif/_48013511_falcon9_226x400.gif';
+      image.alt = 'First successfull Dragon launch';
+    } else if (index === 3) {
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2015/12/23604164970_b573f4d0f4_k-768x512.jpg';
+      image.alt = 'Dragon capsule births with ISS';
+    } else if (index === 4) {
+      image.src = 'http://www.newspacejournal.com/wp-content/uploads/2013/03/dragon-splashdown.jpg';
+      image.alt = 'First Falcon 9 GTO mission';
+    } else if (index == 5) {
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2015/12/f9liftoff-768x478.png';
+      image.alt = 'Successfull Falcon 9 landing';
+    } else if (index === 6) {
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2016/04/26289200916_a0b82385f8_z.jpg';
+      image.alt = 'Successfull Falcon 9 droneship landing'
+    } else if (index === 7) {
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2017/03/IMG_0304-1-copy-678x451.jpg';
+      image.alt = 'First Falcon 9 reflight';
+    } else if (index === 8) {
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2017/03/C8Kw-QSVwAAm8FA-2-678x509.jpg';
+      image.alt = 'Fairing recovered successfully'
+    } else if (index === 9) {
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2017/06/35038651646_4b83ab7dce_k-678x452.jpg';
+      image.alt = 'First Dragon reflight'
+    } else if (index === 10) {
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2018/02/40126461411_a6e49a61f2_k-678x452.jpg';
+      image.alt = 'First Falcon Heavy heliocentric mission'
+    } else if (index === 11) {
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2019/03/WS-SpaceX-Dragon-DM1-0121-1.jpeg';
+      image.alt = 'Successfull Dragon 2 docking with ISS'
+    
+    } else if(index === 12){
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2018/02/25254688767_b67e0bf2ac_k.jpg';
+      image.alt = 'First flight of Raptor engine'
+    
+    } else if(index === 13){
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2017/06/DBap58FUMAAOFU8-678x452.jpg';
+      image.alt = 'First Falcon 9 fairing reuse'
+
+    } else if(index === 14){
+      image.src = 'https://spaceflightnow.com/wp-content/uploads/2020/05/NASA-DM2-7748.jpeg';
+      image.alt = 'SpaceX successfully launches humans to ISS'
+    }
+     else {
       image.src = obtenerImagenAleatoria(); // Función para obtener una imagen
       image.alt = 'Imagen del evento de SpaceX';
     }
@@ -67,6 +107,8 @@ function mostrarHistoria(historia) {
     historySection.appendChild(eventDiv); 
   });
 }
+
+
 
 
 
